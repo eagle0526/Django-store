f868e19a251fff6fe24fc1f17355e902966a3321
Yee <a034506618@gmail.com>
2023-09-04 14:17:15 +0800

    DAY8

--- store/online/models.py
+++ store/online/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+
+class Employer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    
+    def get_absolute_url(self):
+        return reverse('employer-detail', arg=[str(self.id)])
+    
+    def __str__(self):
+        return f'{self.first_name}-{self.last_name}'
+
+
+
+
+
+
+
+
+
+
