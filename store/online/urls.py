9cff6170a34ad8b7732720bc9e3adb0aa2a1081d
Yee <a034506618@gmail.com>
2023-09-04 16:59:30 +0800

    DAY9

--- store/online/urls.py
+++ store/online/urls.py
@@ -5,3 +5,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index")
 ]
+
+
+
+
+urlpatterns += [
+    path("employers/", views.EmployerListView.as_view(), name='employer-list')
+]
+
+
--- store/online/views.py
+++ store/online/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employer
+from django.views import generic
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html")
+
+
+
+class EmployerListView(generic.ListView):
+    model = Employer
