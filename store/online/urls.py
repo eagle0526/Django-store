51c050cdacbc2d2540e4792afcdaab7d94d4c134
Yee <a034506618@gmail.com>
2023-09-05 15:50:17 +0800

    DAY10

--- store/online/urls.py
+++ store/online/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 ]
 
 
-
-
 urlpatterns += [
-    path("employers/", views.EmployerListView.as_view(), name='employer-list')
+    path("employers/", views.EmployerListView.as_view(), name='employer-list'),
+    path("employer/create/", views.EmployerCreate.as_view(), name='employer-create'),
+
 ]
 
 
--- store/online/views.py
+++ store/online/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Employer
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -12,3 +14,11 @@ def index(request):
 
 class EmployerListView(generic.ListView):
     model = Employer
+
+class EmployerCreate(CreateView):
+    model = Employer
+    fields = '__all__'
+    success_url = reverse_lazy('employer-list')
+
+
+
