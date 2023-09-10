4bbebafff497b5f39191363814f1b102b7a56052
Yee <a034506618@gmail.com>
2023-09-10 14:28:51 +0800

    DAY25

--- store/online/admin.py
+++ store/online/admin.py
@@ -3,15 +3,30 @@ from .models import Employer, Store, Product
 # Register your models here.
 
 
+
+
+
 class EmployerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'age')
+    list_filter = ('first_name', 'age')
+
+    fieldsets = (
+        ('name', {
+            'fields': ('first_name', 'last_name')
+        }),
+    )
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'employer')
+    
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'display_store')
+    
+    
+
+
 
 admin.site.register(Employer, EmployerAdmin)
